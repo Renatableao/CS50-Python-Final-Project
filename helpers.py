834ebcbc199382339d481a580e38f9e971247c33
Renata <renatab_leao@yahoo.com.br>
2023-05-02 22:26:03 -0300

    install bugs

--- helpers.py
+++ helpers.py
@@ -23,7 +23,7 @@ def main():
     # print(search("BR", "pt-BR", "BRL", querylegs, 2, [], "CABIN_CLASS_ECONOMY"))
 
     # call api to update market.json list
-    # get_market()
+    print(get_market())
 
     # call api to update currency.json list
     # get_currency()
